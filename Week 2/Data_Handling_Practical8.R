380390afdef828e1a87bdd8877f3f567de9faafa
DumMai <dumrong_m@yahoo.com>
2025-01-07 14:19:58 +0700

    Delete Week 2/Data_Handling_Practical8.R

--- Week 2/Data_Handling_Practical8.R
+++
@@ -1,9 +0,0 @@
-#install.packages("lme4",dep = T)
-library(lme4)
-library(reshape2)
-
-?sleepstudy
-View(sleepstudy)
-
-#####Your tasks
-# Use 'dcast' to create a wide format table for 'sleepstudy' data
